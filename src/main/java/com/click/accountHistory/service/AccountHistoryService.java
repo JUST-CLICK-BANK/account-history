78ad6113d40cda5b6168320e2862de4ca850bba5
Kim JaeMin <2098kjm@naver.com>
2024-07-09 23:48:15 +0900

    [Feat] Post Method, 입금 & 출금 기록 추가

--- src/main/java/com/click/accountHistory/service/AccountHistoryService.java
+++ src/main/java/com/click/accountHistory/service/AccountHistoryService.java
@@ -4,9 +4,17 @@ import com.click.accountHistory.domain.dto.response.AccountHistoryDetailResponse
 import com.click.accountHistory.domain.dto.response.AccountHistoryResponse;
 import java.util.List;
 
+import com.click.accountHistory.domain.dto.request.DepositRequest;
+import com.click.accountHistory.domain.dto.request.WithdrawRequest;
+import java.util.UUID;
+
 public interface AccountHistoryService {
 
     List<AccountHistoryResponse> getAllHistory(String account);
 
     AccountHistoryDetailResponse getHistoryDetail(Long historyId);
+
+    void addDeposit(UUID userId, DepositRequest depositRequest);
+
+    void addWithdraw(UUID userId, WithdrawRequest withdrawRequest);
 }
--- src/main/java/com/click/accountHistory/service/AccountHistoryServiceImpl.java
+++ src/main/java/com/click/accountHistory/service/AccountHistoryServiceImpl.java
@@ -1,11 +1,14 @@
 package com.click.accountHistory.service;
 
+import com.click.accountHistory.domain.dto.request.DepositRequest;
+import com.click.accountHistory.domain.dto.request.WithdrawRequest;
 import com.click.accountHistory.domain.dto.response.AccountHistoryDetailResponse;
 import com.click.accountHistory.domain.dto.response.AccountHistoryResponse;
 import com.click.accountHistory.domain.entity.AccountHistory;
 import com.click.accountHistory.domain.repository.AccountHistoryRepository;
 import java.util.List;
 import java.util.Optional;
+import java.util.UUID;
 import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -29,5 +32,15 @@ public class AccountHistoryServiceImpl implements AccountHistoryService {
         Optional<AccountHistory> byId = accountHistoryRepository.findById(historyId);
         AccountHistory accountHistory = byId.orElseThrow(() -> new IllegalArgumentException("거래 기록이 없습니다."));
         return AccountHistoryDetailResponse.from(accountHistory);
+    }
+
+    @Override
+    public void addDeposit(UUID userId, DepositRequest depositRequest) {
+        accountHistoryRepository.save(depositRequest.toEntity());
+    }
+
+    @Override
+    public void addWithdraw(UUID userId, WithdrawRequest withdrawRequest) {
+        accountHistoryRepository.save(withdrawRequest.toEntity());
     }
 }
