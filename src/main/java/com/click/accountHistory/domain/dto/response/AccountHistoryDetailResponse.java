8c3c4bfa3c316bb141cd43a8c70ad9403e2b727f
Kim JaeMin <2098kjm@naver.com>
2024-07-13 23:49:51 +0900

    [Feat] 결제유형 Enum 처리

--- src/main/java/com/click/accountHistory/domain/dto/response/AccountHistoryDetailResponse.java
+++ src/main/java/com/click/accountHistory/domain/dto/response/AccountHistoryDetailResponse.java
@@ -1,12 +1,11 @@
 package com.click.accountHistory.domain.dto.response;
 
 import com.click.accountHistory.domain.entity.AccountHistory;
-import com.click.accountHistory.domain.entity.Category;
-import java.time.LocalDateTime;
+import com.click.accountHistory.domain.type.TransactionType;
 
 public record AccountHistoryDetailResponse(
     Long historyId,
-    Integer bhOutType,
+    String bhOutType,
     Long cardId,
     String bhReceive,
     String bhMemo
@@ -14,7 +13,7 @@ public record AccountHistoryDetailResponse(
     public static AccountHistoryDetailResponse from(AccountHistory accountHistory) {
         return new AccountHistoryDetailResponse(
             accountHistory.getHistoryId(),
-            accountHistory.getBhOutType(),
+            accountHistory.getBhOutType().getName(),
             accountHistory.getCardId(),
             accountHistory.getBhReceive(),
             accountHistory.getBhMemo()
