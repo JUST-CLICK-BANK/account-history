2437b976df05b762dc3d75889e201b1bb56443e3
Kim JaeMin <2098kjm@naver.com>
2024-07-23 10:12:09 +0900

    [Fix] bhReceive 컬럼 삭제

--- src/main/java/com/click/accountHistory/domain/dto/request/DepositRequest.java
+++ src/main/java/com/click/accountHistory/domain/dto/request/DepositRequest.java
@@ -12,7 +12,6 @@ public record DepositRequest(
     String yourAccount, // 입금에서 내 통장에 표시되는 내용은 받는 분 표시라서 이게 필요한지 모르겠음
     String bhStatus,
     Long bhBalance,
-    String bhReceive,
     Integer categoryId
 ) {
 
@@ -27,7 +26,6 @@ public record DepositRequest(
             .bhBalance(bhBalance)
             .bhOutType(TransactionType.DEPOSIT)
             .cardId(null)
-            .bhReceive(bhReceive)
             .bhMemo(null)
             .categoryId(category)
             .build();
