e9b1b21cf8a83ba3cb249cefcb15dae01585c23e
Kim JaeMin <2098kjm@naver.com>
2024-08-12 20:53:26 +0900

    [Fix] 카테고리 수정 시, 지출 내역 안바뀌는 버그 수정

--- src/main/java/com/click/accountHistory/service/AccountHistoryCategoryServiceImpl.java
+++ src/main/java/com/click/accountHistory/service/AccountHistoryCategoryServiceImpl.java
@@ -1,8 +1,10 @@
 package com.click.accountHistory.service;
 
 import com.click.accountHistory.domain.entity.AccountHistory;
+import com.click.accountHistory.domain.entity.AmountByCategory;
 import com.click.accountHistory.domain.entity.Category;
 import com.click.accountHistory.domain.repository.AccountHistoryRepository;
+import com.click.accountHistory.domain.repository.AmountByCategoryRepository;
 import com.click.accountHistory.domain.repository.CategoryRepository;
 import com.click.accountHistory.exception.AccountHistoryErrorCode;
 import com.click.accountHistory.exception.AccountHistoryException;
@@ -17,6 +19,7 @@ public class AccountHistoryCategoryServiceImpl implements AccountHistoryCategory
 
     private final AccountHistoryRepository accountHistoryRepository;
     private final CategoryRepository categoryRepository;
+    private final AmountByCategoryRepository amountByCategoryRepository;
 
     @Transactional
     @Override
@@ -24,10 +27,31 @@ public class AccountHistoryCategoryServiceImpl implements AccountHistoryCategory
         Optional<AccountHistory> byId = accountHistoryRepository.findById(historyId);
         AccountHistory accountHistory = byId.orElseThrow(
             () -> new AccountHistoryException(AccountHistoryErrorCode.NO_ACCOUNT_HISTORY));
+
+        Category before = accountHistory.getCategoryId();
+        Long amount = accountHistory.getBhAmount();
+
         Optional<Category> byCategoryId = categoryRepository.findById(categoryId);
         Category category = byCategoryId.orElseThrow(
             () -> new AccountHistoryException(AccountHistoryErrorCode.NO_CATEGORY));
 
         accountHistory.setCategoryId(category);
+
+        // 카테고리 수정 시, 지출 데이터 수정
+        AmountByCategory byCategory = amountByCategoryRepository.findByAbcAccountAndAbcCategoryAndAbcDisableTrue(
+            accountHistory.getMyAccount(), before.getCategoryName());
+
+        byCategory.setAbcAmount(byCategory.getAbcAmount() - amount);
+
+        AmountByCategory amountByCategory = amountByCategoryRepository.findByAbcAccountAndAbcCategory(
+            accountHistory.getMyAccount(), category.getCategoryName());
+
+        if (amountByCategory == null && category.getCategoryId() != 9) {
+            AmountByCategory newData = new AmountByCategory(null, accountHistory.getMyAccount(), category.getCategoryName(), amount, true);
+            amountByCategoryRepository.save(newData);
+        } else if (category.getCategoryId() != 9){
+            amountByCategory.setAbcAmount(amountByCategory.getAbcAmount() + amount);
+        }
+
     }
 }
